370ec586ccf926cf015b768da957804d918bcdd0
Matt Slater <github@mslater.fastmail.com>
2023-04-02 17:01:56 +0100

    Add i18n support

--- src/App.hx
+++ src/App.hx
@@ -1,9 +1,15 @@
 package;
 
+
+#if cpp
+import hx.widgets.Clipboard;
+#end
 #if js
 import js.html.Navigator;
 import js.html.Clipboard;
 #end
+import haxe.ui.locale.LocaleManager;
+import haxe.ui.containers.dialogs.Dialogs;
 import haxe.ui.events.MouseEvent;
 import haxe.ui.containers.VBox;
 import Math;
@@ -21,79 +27,72 @@ class App extends VBox {
 
     public function new() {
         super();
+        // LocaleManager.instance.addStrings("en",new Map<String,String>(),"assets/en.properties");
+        haxe.ui.locale.LocaleManager.instance.language = "en";
         // js.Browser.document.getElementById("appContainer").appendChild(this.element);
-        textField.text = "Et sequi voluptatum voluptatibus. Velit doloremque ipsum autem perspiciatis. Et voluptatem est dolor. Voluptatem molestiae est earum. Repellendus et aut animi impedit. Quia velit quis quas at suscipit.\nSunt aliquid perspiciatis sit sit. Veniam facere culpa excepturi facere accusamus omnis quis ex. Vitae iusto et occaecati. Laborum aut molestiae possimus.\nSit quisquam enim sed natus autem. Veritatis est totam qui debitis tempore. Dolorem aliquid iusto omnis et et.";
-        pageCountLabel.hidden = true;
-        errorLabel.hidden = true;
+        inputTextfield.text = "Et sequi voluptatum voluptatibus. Velit doloremque ipsum autem perspiciatis. Et voluptatem est dolor. Voluptatem molestiae est earum. Repellendus et aut animi impedit. Quia velit quis quas at suscipit.\nSunt aliquid perspiciatis sit sit. Veniam facere culpa excepturi facere accusamus omnis quis ex. Vitae iusto et occaecati. Laborum aut molestiae possimus.\nSit quisquam enim sed natus autem. Veritatis est totam qui debitis tempore. Dolorem aliquid iusto omnis et et.";
+        // pageCountLabel.hidden = true;
+        // errorLabel.hidden = true;
     }
 
-    @:bind(button2, MouseEvent.CLICK)
-    private function onMyButton(e:MouseEvent) {
-        splitText = getSplitLines(textField.text, pageCount);
-        textField2.text = splitText[currentPage];
-        pageCountLabel.hidden = false;
+    @:bind(convertBookButton, MouseEvent.CLICK)
+    private function onConvertBookButton(e:MouseEvent) {
+        splitText = getSplitLines(inputTextfield.text, pageCount);
+        outputTextfield.text = splitText[currentPage];
+        // LocaleManager.instance.refreshAll();
         updatePages();
     }
 
-    @:bind(previousPageButton, MouseEvent.CLICK)
-    private function onPreviousPageButton(e:MouseEvent) {
+    @:bind(firstPageButton, MouseEvent.CLICK)
+    private function onFirstCopyButton(e:MouseEvent) {
         if (splitText != null)
         {
-            currentPage--;
+            currentPage = 0;
             updatePages();
         }
+        copyOutputText();
     }
 
-    @:bind(previousCopyButton, MouseEvent.CLICK)
+    @:bind(previousPageButton, MouseEvent.CLICK)
     private function onPreviousCopyButton(e:MouseEvent) {
         if (splitText != null)
         {
             currentPage--;
             updatePages();
         }
-        #if js
-        js.Browser.navigator.clipboard.writeText(textField2.text).then((v) -> {
-            trace('Copied to clipboard!');
-        },
-        (v)-> {
-            trace('Clipboard failed...');
-        });
-        #end
-        #if cpp
-        // 
-        #end
+        copyOutputText();
+    }
+
+    @:bind(copyPageButton, MouseEvent.CLICK)
+    private function onCopyPageButton(e:MouseEvent) {
+        copyOutputText();
     }
 
     @:bind(nextPageButton, MouseEvent.CLICK)
-    private function onNextPageButton(e:MouseEvent) {
-        if (splitText != null)
-        {
-            currentPage++;
-            updatePages();
-        }
-    }
-
-    @:bind(nextCopyButton, MouseEvent.CLICK)
     private function onNextCopyButton(e:MouseEvent) {
         if (splitText != null)
         {
             currentPage++;
             updatePages();
         }
-        #if js
-        js.Browser.navigator.clipboard.writeText(textField2.text).then((v) -> {
-            trace('Copied to clipboard!');
-        },
-        (v)-> {
-            trace('Clipboard failed...');
-        });
-        #end
+        copyOutputText();
+    }
+
+    @:bind(lastPageButton, MouseEvent.CLICK)
+    private function onLastCopyButton(e:MouseEvent) {
+        if (splitText != null)
+        {
+            currentPage = pageCount - 1;
+            updatePages();
+        }
+        copyOutputText();
     }
 
     function updatePages() {
         currentPage = cast Math.max(0,Math.min(currentPage,pageCount));
-        pageCountLabel.text = 'Page ${currentPage+1} of ${pageCount+1}';
-        textField2.text = splitText[currentPage];
+        // pageCountLabel.text = 'Page ${currentPage+1} of ${pageCount+1}';
+        outputTextfield.text = splitText[currentPage];
+        LocaleManager.instance.refreshAll();
     }
 
     /**
@@ -215,23 +214,17 @@ class App extends VBox {
         pageCount = this.pageCount;
         if (this.pageCount > 50)
         {
-            errorLabel.hidden = false;
-            errorLabel.text = "This text will be too long to paste into one book in Bedrock.";
-            pageCountLabel.customStyle.color = 0xFF8600;
-            pageCountLabel.invalidateComponentStyle();
+            errorLabel.customStyle.fontBold = true;
+            errorLabel.text = LocaleManager.instance.lookupString("UI.error.tooLongBedrockOnly");
             if (this.pageCount > 100)
             {
-                errorLabel.text = "This text will be too long to paste into one book in both Java and Bedrock.";
-                pageCountLabel.customStyle.color = 0xFF0000;
-                pageCountLabel.invalidateComponentStyle();
-            }
+                errorLabel.text = LocaleManager.instance.lookupString("UI.error.tooLongBoth");
+                }
         }
-        else
-        {
-            errorLabel.hidden = true;
-            pageCountLabel.customStyle.color = 0x000000;
-            pageCountLabel.invalidateComponentStyle();
+        else {
+            errorLabel.customStyle.fontBold = false;
         }
+
         trace("\n=== DONE ===\n");
         return pages;
     }
@@ -284,5 +277,32 @@ class App extends VBox {
                     return 5;
                 }
         }
+    }
+
+    function copyOutputText() {
+        #if js
+        js.Browser.navigator.clipboard.writeText(outputTextfield.text).then((v) -> {
+            trace('Copied to clipboard!');
+        },
+        (v)-> {
+            trace('Clipboard failed...');
+        });
+        #end
+        #if cpp
+        Clipboard.textData = outputTextfield.text;
+        #end
+    }
+
+    @:bind(openFileMenuItem, MouseEvent.CLICK)
+    private function openTextFile(e:haxe.ui.events.UIEvent) {
+        trace('Attempting to open file dialogue');
+        Dialogs.openTextFile(LocaleManager.instance.lookupString("menu.file.openFile"),[{extension: ".txt", label: LocaleManager.instance.lookupString("menu.file.textFile")}],
+            (info) -> {
+                try {
+                    inputTextfield.text = info.text;
+                } catch (e) {
+                    errorLabel.text = e.message;
+                }
+            });
     }
 }
